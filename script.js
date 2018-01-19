95f2fdc56b7478fd792cea5528926bf93d63b6ff
dylan delancie <dylandelancie@students.berkeley.net>
2018-01-19 11:38:53 -0800

    jagwire team

--- script.js
+++ script.js
@@ -15,27 +15,26 @@ function calculateCurrentGrade(){
         document.getElementById("error").innerHTML = "ERROR, the weights need to add up to 100.";
     }
     var hwGrade = Math.round(avg(convertArrayStringToNumber(document.getElementById("homework").value)));
-    console.log(hwGrade);
     document.getElementById("avgHw").innerHTML = "Your total homework grade is " +  hwGrade + "%";
     colorGrade(document.getElementById("avgHw"), hwGrade);
+
     var quizGrade = Math.round(avg(convertArrayStringToNumber(document.getElementById("quiz").value)));
-    console.log(quizGrade);
     document.getElementById("avgQuiz").innerHTML = "Your total quiz grade is " + quizGrade + "%";
     colorGrade(document.getElementById("avgQuiz"), quizGrade);
+
     var testGrade = Math.round(avg(convertArrayStringToNumber(document.getElementById("test").value)));
-    console.log(testGrade);
     document.getElementById("avgTest").innerHTML = "Your total test grade is " + testGrade + "%";
     colorGrade(document.getElementById("avgTest"), testGrade);
+
     var midtermGrade = Math.round(avg(convertArrayStringToNumber(document.getElementById("midterm").value)));
-    console.log(midtermGrade);
     document.getElementById("avgMidterm").innerHTML = "Your total midterm grade is " + midtermGrade + "%";
     colorGrade(document.getElementById("avgMidterm"), midtermGrade);
+
     var hw = (hwGrade * hwW) / 100;
     var quiz = (quizGrade * qW) / 100;
     var test = (testGrade * tW) / 100;
     var midterm = (midtermGrade * mW) / 100;
     var currentGrade = Math.round(100 * ((hw + quiz + test + midterm) / (100 - fW)));
-    console.log(currentGrade);
     document.getElementById("totalGrade").innerHTML = "Your current grade is a " + currentGrade + "%";
     colorGrade(document.getElementById("totalGrade"), currentGrade);
     calculateGradeNeeded(currentGrade);
@@ -45,7 +44,7 @@ function calculateGradeNeeded(g){
     var x = (1-((document.getElementById("fWeight").value)/100)) * (g/100);
     var y = ((document.getElementById("dGrade").value)/100) - x;
     var finalGradeNeeded = Math.round((y / ((document.getElementById("fWeight").value)/100)) * 100);
-    console.log(finalGradeNeeded);
+
     if(finalGradeNeeded >= 100){
         return document.getElementById("gradeNeeded").innerHTML = "You need a "  +  finalGradeNeeded  +  "% on the final to get your desired grade. Better luck next semester!";
     }
