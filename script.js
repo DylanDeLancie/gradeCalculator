61b77b6b5c18a6f17574afe27a673900b9c39d5c
Dylan De Lancie <dylandelancie@students.berkeley.net>
2019-03-28 18:49:57 -0700

    new

--- script.js
+++ script.js
@@ -1,109 +1,81 @@
 
-
-function calculateCurrentGrade(){
-    document.getElementById("error").innerHTML =  "";
-    var hwW = parseInt(document.getElementById("hwWeight").value);
-    var qW = parseInt(document.getElementById("qWeight").value);
-    var tW = parseInt(document.getElementById("tWeight").value);
-    var mW = parseInt(document.getElementById("mWeight").value);
-    var fW = parseInt(document.getElementById("fWeight").value);
-    var weights = (hwW + qW +tW + mW + fW)/ 100;
-    if(weights > 1){
-        document.getElementById("error").innerHTML = "ERROR, the weights need to add up to 100.";
-    }
-    if(weights < 1){
-        document.getElementById("error").innerHTML = "ERROR, the weights need to add up to 100.";
-    }
-    var hwGrade = Math.round(avg(convertArrayStringToNumber(document.getElementById("homework").value)));
-    document.getElementById("avgHw").innerHTML = "Your total homework grade is " +  hwGrade + "%";
-    colorGrade(document.getElementById("avgHw"), hwGrade);
-
-    var quizGrade = Math.round(avg(convertArrayStringToNumber(document.getElementById("quiz").value)));
-    document.getElementById("avgQuiz").innerHTML = "Your total quiz grade is " + quizGrade + "%";
-    colorGrade(document.getElementById("avgQuiz"), quizGrade);
-
-    var testGrade = Math.round(avg(convertArrayStringToNumber(document.getElementById("test").value)));
-    document.getElementById("avgTest").innerHTML = "Your total test grade is " + testGrade + "%";
-    colorGrade(document.getElementById("avgTest"), testGrade);
-
-    var midtermGrade = Math.round(avg(convertArrayStringToNumber(document.getElementById("midterm").value)));
-    document.getElementById("avgMidterm").innerHTML = "Your total midterm grade is " + midtermGrade + "%";
-    colorGrade(document.getElementById("avgMidterm"), midtermGrade);
-
-    if (hwGrade <= 120){
-        var hw = (hwGrade * hwW) / 100;
-    }
-    if (quizGrade <= 120){
-        var quiz = (quizGrade * qW) / 100;
-    }
-    if (testGrade <= 120){
-        var test = (testGrade * tW) / 100;
-    }
-    if (midtermGrade <= 120){
-        var midterm = (midtermGrade * mW) / 100;
-    }
-    var currentGrade = Math.round(100 * ((hw + quiz + test + midterm) / (100 - fW)));
-    if (isNaN(currentGrade) == true){
-        document.getElementById("totalGrade").innerHTML = "The grades you have entered are not feasible. Please try again";
+function calculateWaterSavings() {
+    var occupants = parseInt(document.getElementById("occupants").value);
+    var ShR = parseInt(document.getElementById("showerRate").value);
+    var ShN = parseInt(document.getElementById("showerNumber").value);
+    var KsR = parseInt(document.getElementById("kitchenRate").value);
+    var KsN = parseInt(document.getElementById("kitchenNumber").value);
+    var LR = parseInt(document.getElementById("lavatoryRate").value);
+    var LN = parseInt(document.getElementById("lavatoryNumber").value);
+    var TR = parseInt(document.getElementById("toiletRate").value);
+    var TN = parseInt(document.getElementById("toiletNumber").value);
+    totalUsage = 0;
+    if (isNaN(occupants)) {
+        document.getElementById("occupantError").innerHTML = "You did not enter a number. Please try again";
     } else {
-        document.getElementById("totalGrade").innerHTML = "Your current grade is a " + currentGrade + "%";
-    }
-    colorGrade(document.getElementById("totalGrade"), currentGrade);
-    calculateGradeNeeded(currentGrade);
-}
+        document.getElementById("occupantError").innerHTML = "";
 
-function calculateGradeNeeded(g){
-    var x = (1-((document.getElementById("fWeight").value)/100)) * (g/100);
-    var y = ((document.getElementById("dGrade").value)/100) - x;
-    var finalGradeNeeded = Math.round((y / ((document.getElementById("fWeight").value)/100)) * 100);
-    if (isNaN(finalGradeNeeded) == true){
-        return document.getElementById("gradeNeeded").innerHTML = "Enter real grades"
-    }
-    if(finalGradeNeeded >= 100){
-        return document.getElementById("gradeNeeded").innerHTML = "You need a "  +  finalGradeNeeded  +  "% on the final to get your desired grade. Better luck next semester!";
-    }
-    document.getElementById("gradeNeeded").innerHTML = "You need a "  +  finalGradeNeeded  +  "% on the final to get your desired grade.";
-}
-
-function convertArrayStringToNumber(string){
-    var array = string.split(",");
-    for(var a = 0; a < array.length; a++){
-        if(isNaN(array[a]) == true){
-            return document.getElementById("error").innerHTML= "ERROR, it seems you have entered an invalid character.";
+        if (isNaN(ShR) == true || isNaN(ShN) == true) {
+            document.getElementById("showerUsage").innerHTML = "You did not enter a number. Please try again";
+        } else {
+            if (ShR <= 2) {
+                document.getElementById("showerUsage").innerHTML = "Your shower head is up to current guidelines!";
+            } else {
+                var ShDiff = ShR - 2;
+                var ShUsage = ShDiff * ShN * 8.2 * occupants * 365;
+                document.getElementById("showerUsage").innerHTML = "By replacing your shower head(s) with one within the guidelines of 2 gallons per minute, you could save " + ShUsage + " gallons per year.";
+                totalUsage += ShUsage;
+            }
         }
+        if (isNaN(KsR) == true || isNaN(KsN) == true) {
+            document.getElementById("kitchenUsage").innerHTML = "You did not enter a number. Please try again";
+        } else {
+            if (KsR <= 1.8) {
+                document.getElementById("kitchenUsage").innerHTML = "Your kitchen faucet is up to current standards!";
+            } else {
+                var KsDiff = KsR - 1.8;
+                var KsUsage = KsDiff * KsN * 4 * occupants * 365;
+                document.getElementById("kitchenUsage").innerHTML = "By replacing your kitchen faucet(s) with one within the guidelines of 1.8 gallons per minute, you could save " + KsUsage + " gallons per year.";
+                totalUsage += KsUsage;
+            }
+        }
+        if (isNaN(LR) == true || isNaN(LN) == true) {
+            document.getElementById("lavatoryUsage").innerHTML = "You did not enter a number. Please try again";
+        } else {
+            if (LR <= 1.2) {
+                document.getElementById("lavatoryUsage").innerHTML = "Your lavatory faucet is up to current standards!";
+            } else {
+                var LDiff = LR - 1.2;
+                var LUsage = LDiff * LN * 4 * occupants * 365;
+                document.getElementById("lavatoryUsage").innerHTML = "By replacing your lavatory faucet(s) with one within the guidelines of 1.2 gallons per minute, you could save " + LUsage + " gallons per year.";
+                totalUsage += LUsage;
+            }
+        }
+        if (isNaN(TR) == true || isNaN(TN) == true) {
+            document.getElementById("toiletUsage").innerHTML = "You did not enter a number. Please try again";
+        } else {
+            if (TR <= .5) {
+                document.getElementById("toiletUsage").innerHTML = "Your toilet is up to current standards!";
+            } else {
+                var TDiff = TR - .5;
+                var TUsage = TDiff * TN * 2.5 * occupants * 365;
+                document.getElementById("toiletUsage").innerHTML = "By replacing your toilet(s) with one within the guidelines of .5 gallons per minute, you could save " + TUsage + " gallons per year.";
+                totalUsage += TUsage;
+            }
+        }
+        if (isNaN(totalUsage) == true) {
+            document.getElementById("Usage").innerHTML = "There is an error with the numbers you have entered. Please try again";
+        } else if (totalUsage == 0) {
+            document.getElementById("Usage").innerHTML = "There is an error with the numbers you have entered. Please try again";
+        } else {
+            document.getElementById("Usage").innerHTML = "By replacing all of your outdated water fixtures, you could save a total of " + totalUsage + " gallons per year";
+        }
+
     }
-    for(var i = 0; i < array.length; i++){
-        array[i] = parseInt(array[i]);
-    }
-    return array;
+}
+function calculateMoneySavings(x){
+    var price = document.getElementById("price").value;
+    var cost = price * totalUsage;
+    document.getElementById("finalCost").innerHTML = "These replacements would save you around $" + cost + " per year.";
 }
 
-function avg(array){
-    var average = 0;
-
-    for(var i=0; i < array.length; i++){
-        average += array[i];
-    }
-    return (average/array.length);
-}
-
-function colorGrade(x, grade){
-    if(grade >= 90){
-        x.style.background = "Green";
-    }
-    if(grade >= 80 && grade < 90){
-        x.style.background = "GreenYellow";
-    }
-    if(grade >= 70 && grade < 80){
-        x.style.background = "Yellow";
-    }
-    if(grade >= 60 && grade < 70){
-        x.style.background = "Tomato";
-    }
-    if(grade >= 50 && grade < 60){
-        x.style.background = "Red"
-    }
-    if(grade <= 50){
-        x.style.background = "Red"
-    }
-}
